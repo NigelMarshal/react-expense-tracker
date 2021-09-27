9a7da85bf3ffdeeadcfc213367124a2d02da14c8
Nigel Marshal <nigel566@hotmail.com>
2021-09-27 16:57:27 +0400

    refactor(app): Folder structure setup

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Expenses from "./Components/Expenses";
+import Expenses from "./Components/Expenses/Expenses";
 
 function App() {
   const expenses = [
--- src/Components/Expenses/Expenses.js
+++ src/Components/Expenses/Expenses.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
-import Card from "./Card";
+import Card from "../UI/Card";
 
 function Expenses(props) {
   return (
