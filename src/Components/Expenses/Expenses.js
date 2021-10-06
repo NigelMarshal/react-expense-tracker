6d16fef109187f72429f35628f42c75ec5cbdaac
Nigel Marshal <nigel566@hotmail.com>
2021-10-06 13:28:28 +0400

    feat(chart): Added chart for expenses

--- src/Components/Expenses/Expenses.js
+++ src/Components/Expenses/Expenses.js
@@ -4,6 +4,7 @@ import ExpenseItem from "./ExpenseItem";
 import ExpensesFilter from "./ExpensesFilter";
 import Card from "../UI/Card";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 const Expenses = (props) => {
   //Set state for a default year/filtered year
@@ -22,6 +23,7 @@ const Expenses = (props) => {
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
+      <ExpensesChart expenses={filteredExpenses} />
       <ExpensesList items={filteredExpenses} />
     </Card>
   );
